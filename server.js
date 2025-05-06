30c866b9d071d4e75f8259221e1937f1b9208387
MeGaTrOn0530 <92781102+MeGaTrOn0530@users.noreply.github.com>
2025-05-06 20:49:18 +0500

    Ypangi o'zgarishlar qo‘shidi

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ if (process.env.TELEGRAM_BOT_TOKEN) {
 // CORS sozlamalarini o'zgartirish
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5500", "http://127.0.0.1:5500"],
+    origin: "*", // Barcha manzillardan so'rovlarni qabul qilish
     credentials: true,
   }),
 )
